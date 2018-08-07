c34099fe8f6ab7eb02cb79b9841e787503fb3de2
Bryan Anderson <bryan.texasbobcat@gmail.com>
2018-08-07 15:14:18 -0500

    Part 1 complete

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 console.log("Javascript is alive!");
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('greeting').innerHTML = "Hello, World!";
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+   document.getElementById('essentials').style.backgroundColor = 'yellow';
+        
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    const newImg = new Image();
+    newImg.src = 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif';
+    document.getElementById('greeting').appendChild(newImg);
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    const newList = document.createElement('ul');
+    newList.className = "todo-items"
+    document.body.appendChild(newList);
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    let list = ['make coffee','eat donut','change diapers','drive to work'];
+    for (let i = 0; i < list.length; i++) {
+       let li = document.createElement('li')
+       let liContent = document.createTextNode(list[i]);
+       li.appendChild(liContent);
+       document.getElementsByClassName('todo-items')[0].appendChild(li);
+    }
+})
